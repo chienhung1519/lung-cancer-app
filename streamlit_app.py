4b0fa637eec4705dd4164bfd95454d17593a3c44
chienhung1519 <chienhung1519@gmail.com>
2023-09-15 02:49:10 +0000

    label test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-picture = st.camera_input("Take a picture", label_visibility="hidden")
+picture = st.camera_input("Take a picture", label_visibility="collapsed")
 
 if picture:
     st.image(picture)

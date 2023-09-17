8a63aa87b6943ae0102e03897ca1c4fba42d87b2
chienhung1519 <chienhung1519@gmail.com>
2023-09-17 13:01:31 +0000

    lottie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
 
-picture = st.camera_input("Take a picture", label_visibility="collapsed")
+placeholder = st.empty()
 
-if picture:
-    st.image(picture)
+with placeholder.echo():
+    st_lottie("https://lottie.host/aeb09430-58c1-4cde-9d82-6c22ff410b2e/iWUBJx0EHF.json")
+
+
+# picture = st.camera_input("Take a picture", label_visibility="collapsed")
+
+# if picture:
+#     st.image(picture)

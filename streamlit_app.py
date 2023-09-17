1238b5793031fbb50bc223c2f32bdb13667e3de0
chienhung1519 <chienhung1519@gmail.com>
2023-09-17 13:12:27 +0000

    page1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
 
-placeholder = st.empty()
+placeholder1 = st.empty()
+placeholder2 = st.empty()
 
-with placeholder.container():
+with placeholder1.container():
     # Title
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns([2,6,2])
     with col2:
         st.header("Lun Cancer GPT")
-    
+
+with placeholder2.container():   
     # Vidio
     st_lottie("https://lottie.host/aeb09430-58c1-4cde-9d82-6c22ff410b2e/iWUBJx0EHF.json")
 
-    # Button
-    col1, col2, col3 = st.columns(3)
-    with col2:
-        st.button("Take photo")
+# Button
+col1, col2, col3 = st.columns([3,5,3])
+with col2:
+    st.button("Take photo")
 
 
 # picture = st.camera_input("Take a picture", label_visibility="collapsed")

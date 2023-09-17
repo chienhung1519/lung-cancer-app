3b9b63dde7c2ae68456c775d11e6e28f604ae584
chienhung1519 <chienhung1519@gmail.com>
2023-09-17 13:30:14 +0000

    camera input

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,41 @@
+import base64
+from io import BytesIO
+from pathlib import Path
+from typing import Optional
+
+import streamlit.components.v1 as components
 import streamlit as st
 from streamlit_lottie import st_lottie
+
+# Tell streamlit that there is a component called camera_input_live,
+# and that the code to display that component is in the "frontend" folder
+frontend_dir = (Path(__file__).parent / "frontend").absolute()
+_component_func = components.declare_component(
+    "back_camera_input", path=str(frontend_dir)
+)
+
+def back_camera_input(
+    height: int = 450,
+    width: int = 500,
+    key: Optional[str] = None,    
+) -> Optional[BytesIO]:
+    """
+    Add a descriptive docstring
+    """
+    b64_data: Optional[str] = _component_func(
+        height=height,
+        width=width,
+        key=key,
+    )
+
+    if b64_data is None:
+        return None
+
+    raw_data = b64_data.split(",")[1]  # Strip the data: type prefix
+
+    component_value = BytesIO(base64.b64decode(raw_data))
+
+    return component_value
 
 placeholder1 = st.empty()
 placeholder2 = st.empty()
@@ -13,13 +49,11 @@ with placeholder2.container():
     st_lottie("https://lottie.host/aeb09430-58c1-4cde-9d82-6c22ff410b2e/iWUBJx0EHF.json")
 
 # Button
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.write("     ")
-with col2:
-    center_button = st.button('Take Photo')
-with col3 :
-    st.write("     ")
+home_button = st.button('Take Photo')
+if home_button:
+    placeholder1.empty()
+    placeholder2.empty()
+    image = back_camera_input()
 
 
 
